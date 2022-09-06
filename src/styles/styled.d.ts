79383fa644a0b27fa0c8a97c2706c1bbbe98076b
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-05 23:12:16 -0300

    feat: create light theme

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,20 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+	export interface DefaultTheme {
+		title: string
+
+		colors: ColorsProps
+	}
+}
+
+interface ColorsProps {
+	background: string
+	primary: string
+	accent: string
+	font: string
+}
+
+// bg: 
+// fg
+// font
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,10 @@
+export default {
+	title: 'light',
+
+	colors: {
+		background: '#fff',
+		primary: '#e6e6e6',
+		accent: '#ccc',
+		font: '#424242'
+	}
+}
